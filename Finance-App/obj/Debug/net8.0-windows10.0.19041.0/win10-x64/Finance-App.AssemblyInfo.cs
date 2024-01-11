1a6c990808c771de91abb8e3f8307853bc92f35d
247gaming <125978265+247gaming@users.noreply.github.com>
2024-01-11 09:21:20 +0100

    aktuallisiertes Logo

--- Finance-App/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/Finance-App.AssemblyInfo.cs
+++ Finance-App/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/Finance-App.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Finance-App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+a4cf93419237a040eaf479b81abbda7365093543")]
 [assembly: System.Reflection.AssemblyProductAttribute("Finance-App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Finance-App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
