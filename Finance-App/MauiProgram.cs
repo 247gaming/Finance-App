a9bd78d080e5f2dc74817b24adbc84532da3d109
247gaming <125978265+247gaming@users.noreply.github.com>
2024-01-16 09:11:09 +0100

    push

--- Finance-App/MauiProgram.cs
+++ Finance-App/MauiProgram.cs
@@ -1,25 +1,61 @@
-﻿using Microsoft.Extensions.Logging;
+﻿<?xml version="1.0" encoding="utf-8" ?>
+<ContentPage
+    x:Class="Finance_App.MainPage"
+    xmlns="http://schemas.microsoft.com/dotnet/2021/maui"
+    xmlns:x="http://schemas.microsoft.com/winfx/2009/xaml">
 
-namespace Finance_App
-{
-    public static class MauiProgram
-    {
-        public static MauiApp CreateMauiApp()
-        {
-            var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
+    <ScrollView>
+        <VerticalStackLayout Spacing="10">
+            <Label HorizontalTextAlignment="Center">
+                Test
+            </Label>
+            <Image
+                Aspect="AspectFit"
+                BackgroundColor="{AppThemeBinding Light=Grey,
+                                                  Dark=LightGrey}"
+                HeightRequest="75"
+                Source="wallet.png" />
 
-#if DEBUG
-    		builder.Logging.AddDebug();
-#endif
+            <Label HorizontalTextAlignment="Center">
+                Test
+            </Label>
 
-            return builder.Build();
-        }
-    }
-}
+            <Rectangle
+                Aspect="Fill"
+                Fill="{AppThemeBinding Light=Black,
+                                       Dark=White}"
+                HeightRequest="10" />
+            <Rectangle
+                Aspect="Fill"
+                Fill="{AppThemeBinding Light=Grey,
+                                       Dark=LightGrey}"
+                HeightRequest="300"
+                RadiusX="15"
+                Stroke="{AppThemeBinding Light=Black,
+                                         Dark=White}"
+                StrokeThickness="2" />
+            <Rectangle
+                Aspect="Fill"
+                Fill="{AppThemeBinding Light=Grey,
+                                       Dark=LightGrey}"
+                HeightRequest="300"
+                RadiusX="15"
+                Stroke="{AppThemeBinding Light=Black,
+                                         Dark=White}"
+                StrokeThickness="2" />
+            <Rectangle
+                Aspect="Fill"
+                Fill="{AppThemeBinding Light=Grey,
+                                       Dark=LightGrey}"
+                HeightRequest="300"
+                RadiusX="15"
+                Stroke="{AppThemeBinding Light=Black,
+                                         Dark=White}"
+                StrokeThickness="2" />
+
+
+
+        </VerticalStackLayout>
+    </ScrollView>
+
+</ContentPage>
